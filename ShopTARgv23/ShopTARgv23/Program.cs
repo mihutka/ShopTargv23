d31e2e98c5ee1057894a4c390fa36a1488010ce3
mihutka <misabajandinov@gmail.com>
2024-09-23 15:02:25 +0300

    update1

--- ShopTARgv23/ShopTARgv23/Program.cs
+++ ShopTARgv23/ShopTARgv23/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ShopTARgv23.Data;
+
 namespace ShopTARgv23
 {
     public class Program
@@ -9,7 +12,11 @@ namespace ShopTARgv23
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+
+
+            builder.Services.AddDbContext<ShopTARgv23Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
+            CreateDbIfNotExists(app);
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -31,6 +38,11 @@ namespace ShopTARgv23
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
+        }
+
+        private static void CreateDbIfNotExists(WebApplication app)
+        {
+            throw new NotImplementedException();
         }
     }
 }
