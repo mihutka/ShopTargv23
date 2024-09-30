390dfa722f729dcb63e34406d666d0ef814ec60e
mihutka <misabajandinov@gmail.com>
2024-09-30 16:12:48 +0300

    update create

---
+++ ShopTARgv23/ShopTARgv23.ApplicationServices/Services/FilesServices.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.Extensions.Hosting;
+using ShopTARgv23.Core.Domain;
+using ShopTARgv23.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTARgv23.ApplicationServices.Services
+{
+    public class FilesServices
+    {
+        private readonly IHostEnvironment _webHost;
+
+        public FilesServices
+            (
+           IHostEnvironment webHost
+            )
+        {
+            _webHost = webHost;
+        }
+        public void FilesToApi(SpaceshipDto dto, Spaceship spaceship)
+        {
+            if (dto.Files != null && dto.Files.Count > 0)
+            {
+                if (Directory.Exists(_webHost.ContentRootPath + "\\multipleFileUpload\\"))
+                {
+                    Directory.CreateDirectory(_webHost.ContentRootPath + "\\multipleFileUpload\\");
+                }
+            }
+
+            foreach (var image in dto.Files)
+            {
+                string uploadFolder = Path.Combine(_webHost.ContentRootPath, "multipleFileUpload");
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + image.FileName;
+            }
+        }
+
+
+    }
+}
--- ShopTARgv23/ShopTARgv23.Core/Dto/SpaceshipDto.cs
+++ ShopTARgv23/ShopTARgv23.Core/Dto/SpaceshipDto.cs
@@ -1,8 +1,10 @@
-﻿namespace ShopTARgv23.Core.Dto
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ShopTARgv23.Core.Dto
 {
     public class SpaceshipDto
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public DateTime BuiltDate { get; set; }
@@ -10,8 +12,17 @@
         public int Crew { get; set; }
         public int EnginePower { get; set; }
 
+        public List<IFormFile> Files { get; set; }
+
+        public IEnumerable<FileToApiDto> Image { get; set; }
+        = new List<FileToApiDto>();
+
         //only in db
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
+
+        
     }
+
+    
 }
