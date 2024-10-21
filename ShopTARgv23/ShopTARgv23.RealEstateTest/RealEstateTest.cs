eca3efbbba443d30cae1a87d434dc8e3ea06969e
mihutka <misabajandinov@gmail.com>
2024-10-21 17:33:33 +0300

    testing create

--- ShopTARgv23/ShopTARgv23.RealEstateTest/RealEstateTest.cs
+++ ShopTARgv23/ShopTARgv23.RealEstateTest/RealEstateTest.cs
@@ -1,6 +1,11 @@
 
+using ShopTARgv23.Core.Domain;
 using ShopTARgv23.Core.Dto;
 using ShopTARgv23.Core.ServiceInterface;
+using ShopTARgv23.Data.Migrations;
+using System;
+using System.Drawing;
+using System.Xml;
 
 
 namespace ShopTARgv23.RealEstateTest
@@ -60,6 +65,146 @@ namespace ShopTARgv23.RealEstateTest
             Assert.Equal(correctGuid, guid);
 
         }
+
+
+        [Fact]
+
+        public async Task Should_DeleteByIdRealEstate_WhenDeleteRealestate()
+        {
+            RealEstateDto realEstate = MockRealEstateData();
+
+             var create  = await Svc<IRealEstateServices>().Create(realEstate);
+
+            var result = await Svc<IRealEstateServices>().Delete((Guid)create.Id);
+           
+            Assert.Equal(create.Id, result.Id);
+            
+        }
+
+        [Fact]
+
+        public async Task ShouldNot_DeleteByIdRealeEstate_WhenDidNotDeleteRealEstate()
+        {
+            RealEstateDto realEstate = MockRealEstateData();
+
+            var realEstate1 = await Svc<IRealEstateServices>().Create(realEstate);
+            var realEstate2 = await Svc<IRealEstateServices>().Create(realEstate);
+
+            var result = await Svc<IRealEstateServices>().Delete((Guid)realEstate2.Id);
+            
+
+           Assert.NotEqual(realEstate1.Id, result.Id);
+            
+
+        }
+
+        [Fact]
+
+        public async Task Should_UpdateRealEstate_WhenUpdateData()
+        {
+
+            var guid = Guid.Parse("dad9f4b9-e301-499b-8c3e-40bf95b8d950");
+
+            //uued andmed
+            RealEstateDto realEstate = MockRealEstateData();
+
+            //andmebaasis olevad andmed
+            RealEstateDto domain = new();
+
+            domain.Id = Guid.Parse("dad9f4b9-e301-499b-8c3e-40bf95b8d950");
+            domain.Location = "qwerty";
+            domain.Size = 34;
+            domain.RoomNumber = 1234;
+            domain.BuildingType = "qwerty";
+            domain.CreatedAt = DateTime.UtcNow;
+            domain.ModifiedAt = DateTime.UtcNow;
+
+           await Svc<IRealEstateServices>().Update(realEstate);
+
+
+
+            Assert.Equal(domain.Id, guid);
+            Assert.DoesNotMatch(domain.Location, realEstate.Location);
+            Assert.DoesNotMatch(domain.RoomNumber.ToString(), realEstate.RoomNumber.ToString());
+            Assert.NotEqual(domain.Size, realEstate.Size);
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        [Fact]
+
+        public async Task Should_UpdateRealEstate_WhenUpdateDataVersion2()
+        {
+            RealEstateDto dto = MockRealEstateData();
+
+            var createRealEstate = await Svc<IRealEstateServices>().Create(dto);
+
+            RealEstateDto update = MockRealEstateData2();
+
+            var result = await Svc<IRealEstateServices>().Update(update);
+
+            Assert.NotEqual(update.Size, result.Size);
+            Assert.DoesNotMatch(update.Location, result.Location);
+            Assert.NotEqual(update.ModifiedAt, result.ModifiedAt);
+
+
+
+
+
+
+        }
+
+
+
+
+        private RealEstateDto  MockRealEstateData()
+        {
+            RealEstateDto realEstate = new()
+            {
+                
+                 Location = "asd",
+                 Size = 100,
+                 RoomNumber = 1,
+                 BuildingType = "asd",
+                 CreatedAt = DateTime.Now,
+                 ModifiedAt = DateTime.Now
+            };
+
+            return realEstate;
+        }
+
+        private RealEstateDto MockRealEstateData2()
+        {
+            RealEstateDto realEstate = new()
+            {
+                Location = "dfsdfd",
+                Size = 110,
+                RoomNumber = 2,
+                BuildingType = "sdfsdfds",
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now
+            };
+
+            return realEstate;
+        }
+
+
+
+        
+        
+        
+
+
+
 
 
     }
